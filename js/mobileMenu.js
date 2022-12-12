0f08cad10c3c4df7ed2295c5895e5270c694e7fd
isaiasleibo1 <isaias@leibovich.ar>
2022-12-12 17:10:05 -0300

    .

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,6 +1,12 @@
 const mobileMenuOpener = document.querySelector('.mobileMenuOpener');
 const mobileMenuCloser = document.querySelector('.mobileMenuCloser');
+const mobileMenu = document.querySelector('#mobileMenu');
 mobileMenuOpener.addEventListener('click', mobileMenuOpenerClicked);
+mobileMenuOpener.addEventListener('click', openMobileMenu);
+mobileMenuCloser.addEventListener('click', mobileMenuCloserClicked);
+mobileMenuCloser.addEventListener('click', closeMobileMenu);
+const body = document.querySelector('body'); 
+
 
 function mobileMenuOpenerClicked() {
     
@@ -15,5 +21,44 @@ function mobileMenuOpenerClicked() {
         }, 50);
         
     }, 200)
+    
+}
+
+function mobileMenuCloserClicked() {
+
+    mobileMenuCloser.style.opacity = '0';
+    mobileMenuOpener.style.opacity = '0';
+
+    setTimeout(() => {
+        mobileMenuCloser.style.display = 'none';
+        mobileMenuOpener.style.display = 'inline';
+
+        setTimeout(() => {
+            mobileMenuOpener.style.opacity = '1';
+        }, 50);
+        
+    }, 200)
+}
+
+function openMobileMenu() {
+
+    setTimeout(() => {
+       mobileMenu.style.display = 'block';
+
+       setTimeout(() => {
+        mobileMenu.style.opacity = '0.95';
+       }, 50);
+         
+    }, 200);
+    
+}
+
+function closeMobileMenu() {
+    mobileMenu.style.opacity = '0'; 
+    
+    setTimeout(() => {
+       mobileMenu.style.display = 'none';
+        
+    }, 200);
     
 }
