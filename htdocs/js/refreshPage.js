f7605b716615ab3a58c8b8c1526c9fb91bbcdb53
isaiasleibo1 <isaias@leibovich.ar>
2022-12-23 15:56:13 -0300

    .

--- htdocs/js/refreshPage.js
+++ htdocs/js/refreshPage.js
@@ -1,12 +1,12 @@
-// Esta script recarga la página si el width de la pantalla cambia
-console.log('Atencón!!: Si la proporcón horizontal de la pantalla cambia la página será recargada.');
-console.log('Esto lo hacemos para evitar errores, disculpe las molestias.');
+// // Esta script recarga la página si el width de la pantalla cambia
+// console.log('Atencón!!: Si la proporcón horizontal de la pantalla cambia la página será recargada.');
+// console.log('Esto lo hacemos para evitar errores, disculpe las molestias.');
 
-const windowWidth = window.innerWidth;
+// const windowWidth = window.innerWidth;
 
-setInterval(() => {
-    let windowWidthChecker = window.innerWidth;
-    if(windowWidth != windowWidthChecker) {
-        location.reload();
-    }
-}, 500);
+// setInterval(() => {
+//     let windowWidthChecker = window.innerWidth;
+//     if(windowWidth != windowWidthChecker) {
+//         location.reload();
+//     }
+// }, 500);
